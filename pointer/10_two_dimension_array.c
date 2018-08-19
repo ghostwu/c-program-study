59c43fe65faa68b405c788158edfdbb6ba2f5760
ghostwu <359173352@qq.com>
2018-08-19 09:12:28 +0800

    two dimension array with pointer

--- pointer/10_two_dimension_array.c
+++ pointer/10_two_dimension_array.c
@@ -9,14 +9,13 @@ int main( void ){
 	printf( "num=%p, num+0=%p, num+1=%p\n", num, (num+0), (num+1) );
 	printf( "*num=%p, *(num+0)=%p, *(num+1)=%p\n", *num, *(num+0), *(num+1) );
 	printf( "**(num)=%.2lf, **(num+0)=%.2lf, **(num+1)=%.2lf\n", **num, **(num+0), **(num+1) );
-	/*
 	for( int i = 0; i < 2; i++ ){
 		for( int j = 0; j < 5; j++ ) {
 			//printf( "a[%d][%d]=%.2lf\t", i, j, num[i][j] );	
-			printf( "a[%d][%d]=%.2lf\t", i, j, *( *(num + i) + j ) );
+			//printf( "a[%d][%d]=%.2lf\t", i, j, *( *(num + i) + j ) );
+			printf( "a[%d][%d]=%.2lf\t", i, j, *( num[i] + j ) );
 		}
 		printf( "\n" );
 	}
-	*/
 	return 0;		
 }
