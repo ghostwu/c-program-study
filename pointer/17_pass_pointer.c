3e92ef4e26e8422231faf78e7570d6f76fe864f2
ghostwu <359173352@qq.com>
2018-08-19 20:58:45 +0800

    another method  to get sum of arr

--- pointer/17_pass_pointer.c
+++ pointer/17_pass_pointer.c
@@ -5,11 +5,21 @@
 #include <stdio.h>
 
 int sum( int* arr, int n );
+int sum2( int arr[], int n );
 
 int main( void ){
 	int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 	printf( "1-10的和为%d\n", sum( a, 10 ) );	
+	printf( "1-10的和为%d\n", sum2( a, 10 ) );	
 	return 0;
+}
+
+int sum2( int arr[], int n ) {
+	int total = 0;
+	for( int i = 0; i < n; i++ ){
+		total += arr[i];
+	}
+	return total;
 }
 
 int sum( int* arr, int n ) {
