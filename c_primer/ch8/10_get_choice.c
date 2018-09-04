f7bc5adbee8e18ee0fbcf2f34079a1dfd07467ff
ghostwu <359173352@qq.com>
2018-09-04 23:04:12 +0800

    getchar的使用案例

---
+++ c_primer/ch8/10_get_choice.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+char get_choice( void );
+char get_first( void );
+
+int main( void ){
+	get_choice();
+	return 0;
+}
+char get_choice( void ){
+	int ch;
+	printf( "请做出你的选择\n" );
+	printf( "a、advice					b、bell\n" );
+	printf( "c、count 					d、quit\n" );
+	ch = getchar();
+	while( ( ch < 'a' || ch > 'c' ) && ch != 'q' ){
+		printf( "请输入a,b,c,q中的任意一个字母\n" );	
+		ch = get_first();
+	}
+	return ch;
+}
+
+char get_first( void ) {
+	int ch;
+	ch = getchar();
+	while( getchar() != '\n' ) {
+		continue;
+	}
+	return ch;
+}
+
